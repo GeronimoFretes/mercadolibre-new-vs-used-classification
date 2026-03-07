76d8eec8c09788496c4f271fd7ce2b22549caac3
geronimofretes <gfretes@itba.edu.ar>
2026-03-06 23:37:02 -0300

    add optuna study workflow

---
+++ scripts/build_processed_data.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from pathlib import Path
+
+import os
+import sys
+
+sys.path.append(os.path.abspath("preprocessing"))
+
+import preprocessing.preprocess_pipeline as pp
+from preprocessing.pipeline_io import save_pipeline, load_pipeline
+
+artifacts = Path('artifacts')
+artifacts.mkdir(parents=True, exist_ok=True)
+
+TRAIN_DATA_PATH = 'data/train_data.jsonlines'
+
+PIPELINE_PATH = artifacts / 'prep_v1'
+USE_LOCAL_PIPELINE = True
+
+COLS_TO_DROP_EARLY = ['subtitle','differential_pricing','international_delivery_mode','listing_source','site_id', 'coverage_areas']
+DATE_COLS = ['date_created','last_updated']
+TARGET = "condition"
+
+df_train = pd.read_json(
+    TRAIN_DATA_PATH, 
+    lines=True, 
+    convert_dates=DATE_COLS
+)
+
+df_train = df_train.drop(columns=COLS_TO_DROP_EARLY).copy()
+
+if not USE_LOCAL_PIPELINE:
+    pipe = pp.FeaturePipeline().fit(df_train)
+    save_pipeline(pipe, out_dir=PIPELINE_PATH)
+    
+pipe_loaded, _ = load_pipeline(PIPELINE_PATH) 
+y = df_train[TARGET]           
+df_train_processed = pipe_loaded.transform(df_train)
+df_train_processed['condition'] = y.copy()
+df_train_processed.to_parquet('data/processed_train_data.parquet', index=False)
